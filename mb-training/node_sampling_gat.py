1b9cfa6698884849013234e9062fa846edfc0b41
Hojae <sonhj07@gmail.com>
2024-12-10 20:44:51 +0000

    Clean loading data mechanism on node_sampling

--- mb-training/node_sampling_gat.py
+++ mb-training/node_sampling_gat.py
@@ -40,30 +40,19 @@ header = ['dataset', 'model', 'batch_size', 'num_partition', 'fanout', 'shuffle'
 def write_to_csv(data, file_name):
     file_exists = os.path.isfile(file_name)
     
-    with open(file_name, mode='a', newline='') as file:  # 'a' for append mode
+    with open(file_name, mode='a', newline='') as file:  
         writer = csv.writer(file)
         
-        if not file_exists:  # Write the header if file doesn't exist
+        if not file_exists: 
             writer.writerow(header)
-        writer.writerows(data)  # Append the rows of data
+        writer.writerows(data)  
 
 
 def train(
     proc_id, nprocs, device, g, num_classes, train_idx, val_idx, test_idx, model, use_uva, params
 ):
     
-    # TODO: 
-    # To tune 
-    # reduce num_partitions or increase batch_size
-    
     ds, batch_size, num_partitions, fanout, shuffle, use_ddp, n_gpu, n_layers, n_hidden, num_heads = params
-
-    # sampler = dgl.dataloading.ClusterGCNSampler(
-    #     g,
-    #     num_partitions,
-    #     cache_path='cache/'+str(time.time())[:10]+'.tmp',
-    #     prefetch_ndata=["feat", "label", "train_mask", "val_mask", "test_mask"],
-    # )
 
     sampler = NeighborSampler(
         [fanout for _ in range(n_layers)], prefetch_node_feats=["feat"], prefetch_labels=["label"]
@@ -74,9 +63,6 @@ def train(
     print('graph', g.device, 'node', g.nodes().device, device)
 
     torch.cuda.set_device(device)
-    # Chang points
-    # 1. to puregpu
-    # 2. use_uva=False     
 
     train_dataloader = dgl.dataloading.DataLoader(
         g,
@@ -105,7 +91,6 @@ def train(
         device=device
     )
 
-    
     test_dataloader = dgl.dataloading.DataLoader(
         g,
         test_idx,
@@ -125,14 +110,14 @@ def train(
     tta = 0
     n_epochs = 0
     best_acc_count = 0
-    best_acc_count_thres = (50, 100) #(50, 100)
+    best_acc_count_thres = (50, 100)
     epoch_time = 0
     n_50_epoch = -1
     n_200_epoch = -1
     n_50_tta = -1
     n_200_tta = -1
 
-    stop_training = torch.tensor(0, dtype=torch.int, device=device)  # Shared stop flag, initially set to 0
+    stop_training = torch.tensor(0, dtype=torch.int, device=device)
     
     for epoch in range(10000):
         t0 = time.time()
@@ -243,14 +228,6 @@ def train(
 
 def run(proc_id, nprocs, devices, g, data, mode, params):
     try:
-        # params
-        # batch_size = [20, 50, 100]
-        # num_partition = [100, 250]
-        # shuffle = [False, True]
-        # use_ddp = [False, True]
-        # n_gpu = ['0', '0,1', '0,1,2,3']
-        # n_layers = [2, 4]
-        # n_hidden = [512, 1024] 
         ds, batch_size, num_partitions, fanout, shuffle, use_ddp, n_gpu, n_layers, n_hidden, num_heads = params 
 
         # find corresponding device for my rank
@@ -268,16 +245,11 @@ def run(proc_id, nprocs, devices, g, data, mode, params):
         train_idx = train_idx.to(device)
         val_idx = val_idx.to(device)
         test_idx = test_idx.to(device)
-        # g = g.to(device if mode == "puregpu" else "cpu")
-        # create GraphSAGE model (distributed)
         in_size = g.ndata["feat"].shape[1]
         in_feats = in_size
         n_hidden = n_hidden
         n_layers = n_layers
-        # batch_size, num_partition, shuffle, use_ddp, n_gpus
         n_classes = num_classes
-        #model = SAGE(in_size, n_hidden, num_classes).to(device)
-        # model = GraphSAGE(in_feats, n_hidden, n_classes, n_layers, activation=F.relu, dropout=0.3).to(device)
         model = NSGAT(in_size, num_heads, n_hidden, n_classes, n_layers, dropout=0.5).to(device)
         model = DistributedDataParallel(
             model, device_ids=[device], output_device=device
@@ -297,18 +269,12 @@ def run(proc_id, nprocs, devices, g, data, mode, params):
             use_uva,
             params
         )
-        # layerwise_infer(proc_id, device, g, num_classes, test_idx, model, use_uva)
-        # cleanup process group
     except Exception as e:
         print(f"Exception in process {proc_id}: {e}")
         import traceback
         traceback.print_exc()
     
     finally:
-        # dist.barrier() 
-        # dist.destroy_process_group()  # Ensure the process group is destroyed
-        # torch.cuda.empty_cache()  # Free GPU memory cache
-        #print(f'Process {proc_id} finished and cleaned up')
         print('all run finished')   
 
 if __name__ == "__main__":
@@ -327,185 +293,101 @@ if __name__ == "__main__":
         help="GPU(s) in use. Can be a list of gpu ids for multi-gpu training,"
         " e.g., 0,1,2,3.",
     )
+    
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="pubmed",
+        help="Selection for Dataset "
+    )
+
     args = parser.parse_args()
     assert (
         torch.cuda.is_available()
     ), f"Must have GPUs to enable multi-gpu training."
 
-    # load and preprocess dataset
     print("Loading data")
- 
-    # thread limiting to avoid resource competition
-    
+    if args.dataset == 'pubmed':
+        dataset_details = utils.load_pubmed()
+        model_params = (3, 256, 4)
+        batch_size = 1024
+        partition = 1000
+        fanout = 10
+    elif args.dataset == 'ogbn-arxiv':
+        dataset_details = utils.load_data('ogbn-arxiv')
+        model_params = (2, 512, 2)
+        batch_size = 1024
+        partition = 5000
+        fanout = 10
+    elif args.dataset == 'reddit':
+        dataset_details = utils.load_reddit()
+        model_params = (4, 1024, 2)
+        batch_size = 1024
+        partition = 5000
+        fanout = 10
+    elif args.dataset == 'ogbn-products':
+        dataset_details = utils.load_data('ogbn-products')
+        model_params = (3, 256, 2)
+        batch_size = 1024
+        partition = 3000
+        fanout = 5
+    elif args.dataset == 'orkut':
+        dataset_details = utils.load_data('orkut')
+        model_params = (2, 128, 1)
+        batch_size = 128
+        partition = 5000
+        fanout = 10
 
+    data = (
+        dataset_details.num_classes,
+        dataset_details.train_idx,
+        dataset_details.val_idx,
+        dataset_details.test_idx,
+    )
+    graph = dataset_details[0]
 
-
-    # batch_size = [20, 50, 100]
-    # num_partition = [100, 250, 500, 1000, 2000, 5000, 10000]
-    # shuffle = [True, False]
-    # use_ddp = [True, False]
-    # n_gpu = ['0', '0,1', '0,1,2,3']
-    # n_layers = [2, 4]
-    # n_hidden = [256, 512, 1024]
-
-    # load and preprocess dataset    print("Loading data")
-
-    ogbn_arxiv_dataset = utils.load_data('ogbn-arxiv')
-    ogbn_arxiv_data = (
-            ogbn_arxiv_dataset.num_classes,
-            ogbn_arxiv_dataset.train_idx,
-            ogbn_arxiv_dataset.val_idx,
-            ogbn_arxiv_dataset.test_idx,
-        )
-    ogbn_arxiv_graph = ogbn_arxiv_dataset[0]
- 
-
-    reddit_dataset = utils.load_reddit()
-    reddit_data = (
-            reddit_dataset.num_classes,
-            reddit_dataset.train_idx,
-            reddit_dataset.val_idx,
-            reddit_dataset.test_idx,
-        )
-    reddit_graph = reddit_dataset[0]
-    
-
-    ogbn_product_dataset = utils.load_data('ogbn-products')
-    ogbn_product_data = (
-            ogbn_product_dataset.num_classes,
-            ogbn_product_dataset.train_idx,
-            ogbn_product_dataset.val_idx,
-            ogbn_product_dataset.test_idx,
-        )
-    ogbn_product_graph = ogbn_product_dataset[0]
-
-    orkut_dataset = utils.load_data('orkut')
-    orkut_data = (
-            orkut_dataset.num_classes,
-            orkut_dataset.train_idx,
-            orkut_dataset.val_idx,
-            orkut_dataset.test_idx,
-        )
-    orkut_graph = orkut_dataset[0]
-
-    pubmed_dataset = utils.load_pubmed()
-    pubmed_data = (
-            pubmed_dataset.num_classes,
-            pubmed_dataset.train_idx,
-            pubmed_dataset.val_idx,
-            pubmed_dataset.test_idx,
-        )
-    pubmed_graph = pubmed_dataset[0]
-
-    # ogbn_papers100M_graph, _, n_class = utils.load_data('ogbn-papers100M')
-    # train_ids = torch.nonzero(ogbn_papers100M_graph.ndata['train_mask']).reshape(-1)
-    # valid_ids = torch.nonzero(ogbn_papers100M_graph.ndata['val_mask']).reshape(-1)
-    # test_ids = torch.nonzero(ogbn_papers100M_graph.ndata['test_mask']).reshape(-1)
-    # ogbn_papers100M_data = (
-    #     n_class, train_ids, valid_ids, test_ids
-    # )
-    datasets = { 
-        'pubmed' : 
-        {
-            'graph' : pubmed_graph,
-            'data' : pubmed_data,
-            'model_params' : (3, 256, 4), #(2, 1024, 4),
-            'batch_size': 1024,
-            'partition' : 1000,
-            'fanout' : 10
-        },
-        'ogbn-arxiv' : 
-        {
-            'graph' : ogbn_arxiv_graph,
-            'data' : ogbn_arxiv_data,
-            'model_params' : (2, 512, 2), ##(4, 256, 2),
-            'batch_size': 1024,
-            'partition' : 5000,
-            'fanout' : 10
-        },
-        'reddit' :
-        {
-            'graph' : reddit_graph,
-            'data' : reddit_data,
-            'model_params' : (4, 1024, 2), #(2, 512, 2),
-            'batch_size': 1024,
-            'partition' : 5000,
-            'fanout' : 10
-        },
-        'ogbn_products' :
-        {
-            'graph' : ogbn_product_graph,
-            'data' : ogbn_product_data,
-            'model_params' : (3, 256, 2), #(3, 128, 2),
-            'batch_size': 1024,
-            'partition' : 3000,
-            'fanout' : 5
-        },
-        # 'orkut' : 
-        # {
-        #     'graph' : orkut_graph,
-        #     'data' : orkut_data,
-        #     'model_params' : (2, 128, 1),
-        #     'batch_size': 128,
-        #     'partition' : 5000,
-        #     'fanout' : 10
-        # },
-        
-        # 'ogbn-papers100M' :
-        # {
-        #     'graph' : ogbn_papers100M_graph,
-        #     'data' : ogbn_papers100M_data,
-        #     'model_params' : (2, 128, 1),
-        #     'batch_size': 128,
-        #     'partition' : 5000,
-        #     'fanout' : 2
-        # },
+    dataset = {
+        'graph': graph,
+        'data': data,
+        'model_params': model_params,
+        'batch_size': batch_size,
+        'partition': partition,
+        'fanout': fanout
     }
     os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
 
 
     shuffle = [True]
-    use_ddp = [True]
-    n_gpu = ['0', '0,1', '0,1,2', '0,1,2,3']
-    n_gpu = ['0,1,2,3']
- 
+    use_ddp = [True] 
+    n_gpu = args.gpu
 
     i = 0
-    for ds in datasets.keys():
-        dataset = datasets[ds]
+    l = dataset['model_params'][0]
+    h = dataset['model_params'][1]
+    head = dataset['model_params'][2]
+    b = dataset['batch_size']
+    p = dataset['partition']
+    g = dataset['graph']  # already prepares ndata['label'/'train_mask'/'val_mask'/'test_mask']
+    f = dataset['fanout']
+    if dataset in ["ogbn-arxiv", 'ogbn-papers100M', 'orkut'] :
+        print('converting bidirectional')
+        g.edata.clear()
+        g = dgl.to_bidirected(g, copy_ndata=True)
+        g = dgl.remove_self_loop(g)
+        g = dgl.add_self_loop(g)
+    else:
+        g.edata.clear()
+        g = dgl.remove_self_loop(g)
+        g = dgl.add_self_loop(g)
+    g.create_formats_()
+    data = dataset['data']
 
-        l = dataset['model_params'][0]
-        h = dataset['model_params'][1]
-        head = dataset['model_params'][2]
-        b = dataset['batch_size']
-        p = dataset['partition']
-        g = dataset['graph']  # already prepares ndata['label'/'train_mask'/'val_mask'/'test_mask']
-        f = dataset['fanout']
-        if ds in ["ogbn-arxiv", 'ogbn-papers100M', 'orkut'] :
-            print('converting bidirectional')
-            g.edata.clear()
-            g = dgl.to_bidirected(g, copy_ndata=True)
-            g = dgl.remove_self_loop(g)
-            g = dgl.add_self_loop(g)
-        else:
-            g.edata.clear()
-            g = dgl.remove_self_loop(g)
-            g = dgl.add_self_loop(g)
-        g.create_formats_()
-        data = dataset['data']
-
-        for s in shuffle:
-            for d in use_ddp:
-                for n_g in n_gpu: 
-                    # for p in num_partition:
-                    #     for l in n_layers:
-                    #         for h in n_hidden:
-                    #             for head in n_heads:
-                    
-                    devices = list(map(int, n_g.split(",")))
-                    nprocs = len(devices) 
-                    print(f"{i} th training in {args.mode} mode using {nprocs} GPU(s)")
-                    os.environ["OMP_NUM_THREADS"] = str(mp.cpu_count() // 2 // nprocs)
-                    print('Running parameters : ', ds, b, p, f, s, d, n_g, l, h, head)
-                    mp.spawn(run, args=(nprocs, devices, g, data, args.mode, (ds, b, p, f, s, d, n_g, l, h, head)), nprocs=nprocs)
-                    i+=1
+    for s in shuffle:
+        for d in use_ddp:
+            devices = list(map(int, n_gpu.split(",")))
+            nprocs = len(devices) 
+            print(f"{i} th training in {args.mode} mode using {nprocs} GPU(s)")
+            os.environ["OMP_NUM_THREADS"] = str(mp.cpu_count() // 2 // nprocs)
+            print('Running parameters : ', dataset, b, p, f, s, d, n_gpu, l, h, head)
+            mp.spawn(run, args=(nprocs, devices, g, data, args.mode, (dataset, b, p, f, s, d, n_gpu, l, h, head)), nprocs=nprocs)
+            i+=1
--- mb-training/node_sampling_sage.py
+++ mb-training/node_sampling_sage.py
@@ -29,22 +29,17 @@ header = ['dataset', 'model', 'batch_size', 'num_partition', 'fanout', 'shuffle'
 def write_to_csv(data, file_name):
     file_exists = os.path.isfile(file_name)
     
-    with open(file_name, mode='a', newline='') as file:  # 'a' for append mode
+    with open(file_name, mode='a', newline='') as file:
         writer = csv.writer(file)
         
-        if not file_exists:  # Write the header if file doesn't exist
+        if not file_exists:
             writer.writerow(header)
-        writer.writerows(data)  # Append the rows of data
+        writer.writerows(data)
 
 
 def train(
     proc_id, nprocs, device, g, num_classes, train_idx, val_idx, test_idx, model, use_uva, params
 ):
-    
-    # TODO: 
-    # To tune 
-    # reduce num_partitions or increase batch_size
-    
     ds, batch_size, num_partitions, fanout, shuffle, use_ddp, n_gpu, n_layers, n_hidden = params
  
     sampler = NeighborSampler(
@@ -56,10 +51,6 @@ def train(
     print('graph', g.device, 'node', g.nodes().device, device)
 
     torch.cuda.set_device(device)
-    # Chang points
-    # 1. to puregpu
-    # 2. use_uva=False     
-
 
     train_dataloader = dgl.dataloading.DataLoader(
         g,
@@ -102,18 +93,6 @@ def train(
         device=device
     )
 
-    # val_dataloader = dgl.dataloading.DataLoader(
-    #     g,
-    #     torch.arange(num_partitions).to("cuda"),
-    #     sampler,
-    #     device=device,
-    #     batch_size=100,
-    #     shuffle=True,
-    #     drop_last=False,
-    #     num_workers=0,
-    #     use_ddp=True,
-    #     use_uva=use_uva,
-    # )
     opt = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
     durations = []
     end=0
@@ -128,7 +107,7 @@ def train(
     n_50_tta = -1
     n_200_tta = -1
 
-    stop_training = torch.tensor(0, dtype=torch.int, device=device)  # Shared stop flag, initially set to 0
+    stop_training = torch.tensor(0, dtype=torch.int, device=device)
 
     for epoch in range(10000):
         t0 = time.time()
@@ -136,13 +115,8 @@ def train(
         total_loss = 0
         epoch_start = time.time()
         for it, (_, _,blocks) in enumerate(train_dataloader):
-            # print(end-start)
             x = blocks[0].srcdata["feat"]
             y = blocks[-1].dstdata["label"]
-            # try:
-            #     y = y.type(torch.cuda.LongTensor)
-            # except:
-            #     y = y.to(torch.int64)
 
             y_hat = model(blocks, x)
             loss = F.cross_entropy(y_hat, y)
@@ -239,20 +213,10 @@ def train(
 
 def run(proc_id, nprocs, devices, g, data, mode, params):
     try:
-        # params
-        # batch_size = [20, 50, 100]
-        # num_partition = [100, 250]
-        # shuffle = [False, True]
-        # use_ddp = [False, True]
-        # n_gpu = ['0', '0,1', '0,1,2,3']
-        # n_layers = [2, 4]
-        # n_hidden = [512, 1024] 
         ds, batch_size, num_partition, fanout, shuffle, use_ddp, n_gpu, n_layers, n_hidden = params 
 
-        # find corresponding device for my rank
         device = devices[proc_id]
         torch.cuda.set_device(device)
-        # initialize process group and unpack data for sub-processes
         dist.init_process_group(
             backend="nccl",
             init_method="tcp://127.0.0.1:12352",
@@ -263,20 +227,15 @@ def run(proc_id, nprocs, devices, g, data, mode, params):
         train_idx = train_idx.to(device)
         val_idx = val_idx.to(device)
         test_idx = test_idx.to(device)
-        # g = g.to(device if mode == "puregpu" else "cpu")
-        # create GraphSAGE model (distributed)
         in_size = g.ndata["feat"].shape[1]
         in_feats = in_size
         n_hidden = n_hidden
         n_layers = n_layers
-        # batch_size, num_partition, shuffle, use_ddp, n_gpus
         n_classes = num_classes
-        #model = SAGE(in_size, n_hidden, num_classes).to(device)
         model = NSGraphSAGE(in_feats, n_hidden, n_classes, n_layers, dropout=0.3, activation=F.relu).to(device)
         model = DistributedDataParallel(
             model, device_ids=[device], output_device=device
         )
-        # training + testing
         use_uva = mode == "mixed"
         train(
             proc_id,
@@ -291,8 +250,6 @@ def run(proc_id, nprocs, devices, g, data, mode, params):
             use_uva,
             params
         )
-        # layerwise_infer(proc_id, device, g, num_classes, test_idx, model, use_uva)
-        # cleanup process group
     except Exception as e:
         print(f"Exception in process {proc_id}: {e}")
         import traceback
@@ -318,6 +275,12 @@ if __name__ == "__main__":
         help="GPU(s) in use. Can be a list of gpu ids for multi-gpu training,"
         " e.g., 0,1,2,3.",
     )
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="pubmed",
+        help="Selection for Dataset "
+    )
     args = parser.parse_args()
     assert (
         torch.cuda.is_available()
@@ -325,154 +288,117 @@ if __name__ == "__main__":
 
     # load and preprocess dataset
     print("Loading data")
-  
-    # batch_size = [20, 50, 100]
-    # num_partition = [100, 250, 500, 1000, 2000, 5000, 10000]
-    # shuffle = [True, False]
-    # use_ddp = [True, False]
-    # n_gpu = ['0', '0,1', '0,1,2,3']
-    # n_layers = [2, 4]
-    # n_hidden = [256, 512, 1024]
-
-    # load and preprocess dataset    print("Loading data")
-
-
-    
-    #: pubmed  (3,256), ogbn-arxiv - (2,512), reddit - (3,1024), ogbn-products - (3,256), ogbn-papers100m - (2, 128). ET - Epoch ti
-    ogbn_arxiv_dataset = utils.load_data('ogbn-arxiv')
-    ogbn_arxiv_data = (
-            ogbn_arxiv_dataset.num_classes,
-            ogbn_arxiv_dataset.train_idx,
-            ogbn_arxiv_dataset.val_idx,
-            ogbn_arxiv_dataset.test_idx,
-        )
-    ogbn_arxiv_graph = ogbn_arxiv_dataset[0]
-
-
-    pubmed_dataset = utils.load_pubmed()
-    pubmed_data = (
+    if args.dataset == 'pubmed':
+        pubmed_dataset = utils.load_pubmed()
+        graph = pubmed_dataset[0]
+        data = (
             pubmed_dataset.num_classes,
             pubmed_dataset.train_idx,
             pubmed_dataset.val_idx,
             pubmed_dataset.test_idx,
         )
-    pubmed_graph = pubmed_dataset[0]
+        model_params = (3, 256)
+        batch_size = 20
+        partition = 1000
+        fanout = 10
 
-    reddit_dataset = utils.load_reddit()
-    reddit_data = (
+    elif args.dataset == 'ogbn-arxiv':
+        ogbn_arxiv_dataset = utils.load_data('ogbn-arxiv')
+        graph = ogbn_arxiv_dataset[0]
+        data = (
+            ogbn_arxiv_dataset.num_classes,
+            ogbn_arxiv_dataset.train_idx,
+            ogbn_arxiv_dataset.val_idx,
+            ogbn_arxiv_dataset.test_idx,
+        )
+        model_params = (2, 512)
+        batch_size = 50
+        partition = 10000
+        fanout = 15
+
+    elif args.dataset == 'reddit':
+        reddit_dataset = utils.load_reddit()
+        graph = reddit_dataset[0]
+        data = (
             reddit_dataset.num_classes,
             reddit_dataset.train_idx,
             reddit_dataset.val_idx,
             reddit_dataset.test_idx,
         )
-    reddit_graph = reddit_dataset[0]
-     
-    ogbn_product_dataset = utils.load_data('ogbn-products')
-    ogbn_product_data = (
+        model_params = (3, 1024)
+        batch_size = 100
+        partition = 1000
+        fanout = 20
+
+    elif args.dataset == 'ogbn-products':
+        ogbn_product_dataset = utils.load_data('ogbn-products')
+        graph = ogbn_product_dataset[0]
+        data = (
             ogbn_product_dataset.num_classes,
             ogbn_product_dataset.train_idx,
             ogbn_product_dataset.val_idx,
             ogbn_product_dataset.test_idx,
         )
-    ogbn_product_graph = ogbn_product_dataset[0]
+        model_params = (3, 256)
+        batch_size = 50
+        partition = 2000
+        fanout = 10
 
-    # ogbn_papers100M_graph, _, n_class = utils.load_data('ogbn-papers100M')
-    # train_ids = torch.nonzero(ogbn_papers100M_graph.ndata['train_mask']).reshape(-1)
-    # valid_ids = torch.nonzero(ogbn_papers100M_graph.ndata['val_mask']).reshape(-1)
-    # test_ids = torch.nonzero(ogbn_papers100M_graph.ndata['test_mask']).reshape(-1)
-    # ogbn_papers100M_data = (
-    #     n_class, train_ids, valid_ids, test_ids
-    # )
-    
-    datasets = { 
-        'pubmed' : 
-        {
-            'graph' : pubmed_graph,
-            'data' : pubmed_data,
-            'model_params' : (3, 256), #(2, 256),
-            'batch_size': 1024,
-            'partition' : 1000,
-            'fanout' : 10
-        },
-        'ogbn-arxiv' : 
-        {
-            'graph' : ogbn_arxiv_graph,
-            'data' : ogbn_arxiv_data,
-            'model_params' : (2, 512), #(5, 128),
-            'batch_size': 1024,
-            'partition' : 10000,
-            'fanout' : 20
-        },
-        'reddit' :
-        {
-            'graph' : reddit_graph,
-            'data' : reddit_data,
-            'model_params' : (4, 1024), #(4, 512),
-            'batch_size': 1024,
-            'partition' : 1000,
-            'fanout' : 5
-        },
-        'ogbn_products' :
-        {
-            'graph' : ogbn_product_graph,
-            'data' : ogbn_product_data,
-            'model_params' : (3, 256), #(5, 256),
-            'batch_size': 1024,
-            'partition' : 2000,
-            'fanout' : 5
-        },
-        # 'ogbn-papers100M' :
-        # {
-        #     'graph' : ogbn_papers100M_graph,
-        #     'data' : ogbn_papers100M_data,
-        #     'model_params' : (2, 128),
-        #     'batch_size': 128,
-        #     'partition' : 5000,
-        #     'fanout' : 2
-        # },
+    elif args.dataset == 'ogbn-papers100M':
+        ogbn_papers100M_dataset = utils.load_data('ogbn-papers100M')
+        graph = ogbn_papers100M_dataset[0]
+        train_ids = torch.nonzero(ogbn_papers100M_dataset.ndata['train_mask'], as_tuple=False).reshape(-1)
+        valid_ids = torch.nonzero(ogbn_papers100M_dataset.ndata['val_mask'], as_tuple=False).reshape(-1)
+        test_ids = torch.nonzero(ogbn_papers100M_dataset.ndata['test_mask'], as_tuple=False).reshape(-1)
+        data = (
+            ogbn_papers100M_dataset.num_classes, train_ids, valid_ids, test_ids
+        )
+        model_params = (2, 128)
+        batch_size = 20
+        partition = 5000
+        fanout = 5
+
+    dataset = {
+        'graph': graph,
+        'data': data,
+        'model_params': model_params,
+        'batch_size': batch_size,
+        'partition': partition,
+        'fanout': fanout
     }
+
     os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
  
     shuffle = [True]
-    use_ddp = [True]
-    n_gpu = ['0', '0,1', '0,1,2', '0,1,2,3']
-    n_gpu = ['0,1,2,3']  
-    n_gpu.reverse()
+    use_ddp = [True] 
  
 
     i = 0
-    for ds in datasets.keys():
-        dataset = datasets[ds]
-        l = dataset['model_params'][0]
-        h = dataset['model_params'][1]
-        b = dataset['batch_size']
-        p = dataset['partition']
-        g = dataset['graph']  # already prepares ndata['label'/'train_mask'/'val_mask'/'test_mask']
-        f = dataset['fanout']
-        # avoid creating certain graph formats in each sub-process to save momory
-        if ds in ["ogbn-arxiv", 'ogbn-papers100M', 'orkut'] :
-            print('converting bidirectional')
-            g.edata.clear()
-            g = dgl.to_bidirected(g, copy_ndata=True)
-            g = dgl.remove_self_loop(g)
-            g = dgl.add_self_loop(g)
-        else:
-            g.edata.clear()
-            g = dgl.remove_self_loop(g)
-            g = dgl.add_self_loop(g)
-        g.create_formats_()
-        data = dataset['data']
-        
-        for s in shuffle:
-            for d in use_ddp:
-                for n_g in n_gpu:
-                    # for p in num_partition:
-                    #     for l in n_layers:
-                    #         for h in n_hidden: 
-                    devices = list(map(int, n_g.split(",")))
-                    nprocs = len(devices) 
-                    print(f"{i} th training in {args.mode} mode using {nprocs} GPU(s)")
-                    os.environ["OMP_NUM_THREADS"] = str(mp.cpu_count() // 2 // nprocs)
-                    print('Running parameters : ',ds, b, p, f, s, d, n_g, l, h)
-                    mp.spawn(run, args=(nprocs, devices, g, data, args.mode, (ds, b, p, f, s, d, n_g, l, h)), nprocs=nprocs, join=True)
-                    i+=1    
+    l = dataset['model_params'][0]
+    h = dataset['model_params'][1]
+    b = dataset['batch_size']
+    p = dataset['partition']
+    g = dataset['graph']  # already prepares ndata['label'/'train_mask'/'val_mask'/'test_mask']
+    f = dataset['fanout']
+    if args.dataset in ["ogbn-arxiv", 'ogbn-papers100M', 'orkut'] :
+        print('converting bidirectional')
+        g.edata.clear()
+        g = dgl.to_bidirected(g, copy_ndata=True)
+        g = dgl.remove_self_loop(g)
+        g = dgl.add_self_loop(g)
+    else:
+        g.edata.clear()
+        g = dgl.remove_self_loop(g)
+        g = dgl.add_self_loop(g)
+    g.create_formats_()
+    data = dataset['data']
+    
+    for s in shuffle:
+        for d in use_ddp:
+            devices = list(map(int, args.gpu.split(",")))
+            nprocs = len(devices) 
+            print(f"{i} th training in {args.mode} mode using {nprocs} GPU(s)")
+            os.environ["OMP_NUM_THREADS"] = str(mp.cpu_count() // 2 // nprocs)
+            print('Running parameters : ',args.dataset, b, p, f, s, d, args.gpu, l, h)
+            mp.spawn(run, args=(nprocs, devices, g, data, args.mode, (args.dataset, b, p, f, s, d, args.gpu, l, h)), nprocs=nprocs, join=True)
+            i+=1    
