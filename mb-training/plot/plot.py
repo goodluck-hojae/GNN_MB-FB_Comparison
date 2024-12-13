42f490815aaedf4319b89967ab3a8a8bc4375622
Hojae <sonhj07@gmail.com>
2024-12-13 23:47:37 +0000

    Update

--- mb-training/plot/plot.py
+++ mb-training/plot/plot.py
@@ -9,12 +9,15 @@ args = parser.parse_args()
 with open(args.filename, 'r') as f:
     data = json.load(f)
 
+
+_filename = args.filename.replace('json', '')
+
 test_acc = [item['test_acc'] for item in data]
 times = [item['time'] for item in data]
 cumulative_time = [sum(times[:i+1]) for i in range(len(times))]
 
 plt.figure(figsize=(10, 5))
-plt.plot(cumulative_time, test_acc, label='Test Accuracy', marker='o', linestyle='-', color='green')
+plt.plot(cumulative_time, test_acc, label=_filename, marker='o', linestyle='-', color='green')
 plt.title('Test Accuracy Over Time')
 plt.xlabel('Time (s)')
 plt.ylabel('Test Accuracy')
